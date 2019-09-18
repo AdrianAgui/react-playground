1cd09d1bf744b3d286f18952c6d9a289c249a108
AdrianAgui9 <adrian.aguirre@aktios.com>
2019-09-18 12:44:46 +0200

    landing page prototype

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  state = {
+    logo: true,
+    title: 'Welcome to my React JS Playground'
+  }
+
+  toggleLogo = () => { this.setState({logo: !this.state.logo}) }
+
+  render() {
+
+    return (
+
+      <div className="app">
+          
+        <header className="app-header">
+          <h2 className="mb-5">Welcome to my React Playground</h2>
+        </header>
+        
+        <body className="app-body">
+          <h1 className="mb-5">Body of React playground</h1>
+
+          <button className="btn btn-primary mb-3" onClick={this.toggleLogo}>Toggle React logo</button>
+
+          {
+            this.state.logo ?
+              <img src={logo} className="app-logo" alt="logo" /> : null
+          }
+          
+          <p>
+            <code>{this.state.title}</code>
+          </p>
+
+          <kbd><a
+            className="app-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a></kbd>
+        </body>
+
+        <footer className="app-footer">
+          <code className="text-light mb-5 mr-5">Adrián Aguirre, all rights reserved &#169;</code>
+        </footer>
+
+      </div>
+      
+    );
+
+  }
 }
 
 export default App;
